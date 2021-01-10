94ebe6e92d4a6c45288eb38d98e6c39571eb32ad
3A732045 <jessezzxxcc@gmail.com>
2021-01-11 01:53:21 +0800

    新增 AdminReservationsController ，AdminRoomsController  (index)

--- app/Http/Controllers/AdminReservationController.php
+++ app/Http/Controllers/AdminReservationController.php
@@ -13,7 +13,7 @@ class AdminReservationController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.reservations.index');
     }
 
     /**
--- app/Http/Controllers/AdminRoomController.php
+++ app/Http/Controllers/AdminRoomController.php
@@ -13,7 +13,7 @@ class AdminRoomController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.rooms.index');
     }
 
     /**
