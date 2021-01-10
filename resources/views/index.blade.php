18c9589e38f7a9312caee46aaf844dbfcf64c355
Chen Dong Meng <3a732001@gm.student.ncut.edu.tw>
2021-01-11 00:49:36 +0800

    房型一覽頁面

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 -->
 <html>
 	<head>
-		<title>Hielo by TEMPLATED</title>
+		<title>曙晨民宿</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="assets/css/main.css" />
@@ -25,7 +25,7 @@
 					<li><a href="/">首頁</a></li>
 					<li><a href={{ route('login') }}>登入</a></li>
                     <li><a href={{ route('register') }}>註冊</a></li>
-                    <li><a href=href="#">房型一覽</a></li>
+                    <li><a href="/rooms">房型介紹</a></li>
                     <li><a href=href="#">線上訂房</a></li>
 
 				</ul>
@@ -127,6 +127,44 @@
 								</div>
 							</div>
 						</div>
+
+                        <div>
+                            <div class="box">
+                                <div class="image fit">
+                                    <img src="images/room02.jpg" alt="" />
+                                </div>
+                                <div class="content">
+                                    <header class="align-center">
+                                        <p>DELUXE TWIN</p>
+                                        <h2>豪華雙床房</h2>
+                                    </header>
+                                    <p>大約13坪，內含兩中床，光線明亮、空間寬敞；視野遼闊、獨特景觀設計，高雅紅木傢俱陳設、乾溼分離清爽衛浴空間，高科技通訊設備。2019全面換新之地毯，採「中彩低明度」色調，並以「桂花」圖騰延伸設計；桂花花語為「收穫」，預祝您的每趟旅程都能收穫滿滿，另外也取「桂」諧音，說明每位嘉賓都是我們的「貴人」。</p>
+                                    <footer class="align-center">
+                                        <a href="#" class="button alt">房型一覽</a>
+                                    </footer>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div class="box">
+                                <div class="image fit">
+                                    <img src="images/room03.jpg" alt="" />
+                                </div>
+                                <div class="content">
+                                    <header class="align-center">
+                                        <p>ELEGANT ROOM</p>
+                                        <h2>典雅客房</h2>
+                                    </header>
+                                    <p>約14坪大小的雙人房，整體設計透過降低高彩度色系，減少視覺刺激，並輔上穩重色調之傢俱，定調出場域機能，室內對外窗引入明亮光線，綴以牆面及地毯的銀杏圖樣，促使房內空間更顯生意盎然，讓您能恣意放鬆，享受休閒假期。
+
+                                        房間提供一大床（180cm × 200cm） 及舒適沙發；43吋互動型智慧電視，不論影音、通訊、網路，三合一功能，享受享受高科技帶來的方便生活；客房書桌椅為紅木打造，經改造上漆後，更顯歷史風華；茶點配置均選用在地安心茶款。</p>
+                                    <footer class="align-center">
+                                        <a href="#" class="button alt">房型一覽</a>
+                                    </footer>
+                                </div>
+                            </div>
+                        </div>
 
 					</div>
 				</div>
