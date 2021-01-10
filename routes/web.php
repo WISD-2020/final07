5122e403456b44823ab98df8b0b460a3facaa24a
3A732045 <jessezzxxcc@gmail.com>
2021-01-10 16:37:23 +0800

    新增管理員routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminReservationController;
+use App\Http\Controllers\AdminRoomController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,17 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+
+//管理員
+Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminReservationController::class, 'index'])->name('admin.reservations.index');
+    Route::get('reservations/{id}/edit', [AdminReservationController::class, 'edit'])->name('admin.reservations.edit');
+    Route::post('reservations', [AdminReservationController::class, 'store'])->name('admin.reservations.store');
+    Route::get('reservations', [AdminReservationController::class, 'index'])->name('admin.reservations.index');
+    Route::get('rooms/create', [AdminRoomController::class, 'create'])->name('admin.rooms.create');
+    Route::patch('rooms/{id}', [AdminRoomController::class, 'update'])->name('admin.rooms.update');
+    Route::delete('rooms/{id}', [AdminRoomController::class, 'destroy'])->name('admin.rooms.destroy');
+    Route::get('rooms/{id}/edit', [AdminRoomController::class, 'edit'])->name('admin.rooms.edit');
+});
